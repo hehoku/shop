4f47ab3468162e02ed10af3aa816df2a215d1811
hehoku <hehoku@outlook.com>
2023-12-07 22:09:50 +0800

    Add new product page and layout updates

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: 'https://images.unsplash.com' }],
+  },
+}
 
 module.exports = nextConfig
