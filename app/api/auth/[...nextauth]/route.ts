469cf3f4bef0bf320ff0f808a3fa36f72510fca8
hehoku <hehoku@outlook.com>
2023-12-16 22:10:59 +0800

    chore: merge cart with user's and local

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { mergeAnonymousCartIntoUserCart } from '@/lib/db/cart'
 import { prisma } from '@/lib/db/prisma'
 import { env } from '@/lib/env'
 import { PrismaAdapter } from '@auth/prisma-adapter'
@@ -17,6 +18,17 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id
+      return session
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonymousCartIntoUserCart(user.id)
+    },
+  },
 }
 
 const handler = NextAuth(authOptions)
