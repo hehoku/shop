ed88a5e2bfb034e2168b12c614d07e228217bd92
hehoku <hehoku@outlook.com>
2023-12-17 15:38:08 +0800

    Update Google provider in authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,17 +6,14 @@ import { PrismaClient } from '@prisma/client'
 import { NextAuthOptions } from 'next-auth'
 import { Adapter } from 'next-auth/adapters'
 import NextAuth from 'next-auth/next'
-import Google from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
   providers: [
-    Google({
+    GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
-      httpOptions: {
-        timeout: 40000,
-      },
     }),
   ],
   callbacks: {
